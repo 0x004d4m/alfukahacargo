9ecd9bb5b357f433cc252a79b704bdad80850bc6
0x004d4m <0x004d4m@gmail.com>
2023-01-28 15:03:10 +0300

    updates

--- resources/views/vendor/backpack/crud/columns/json_images.blade.php
+++ resources/views/vendor/backpack/crud/columns/json_images.blade.php
@@ -5,9 +5,9 @@
 @endphp
 @includeWhen(!empty($column['wrapper']), 'crud::columns.inc.wrapper_start')
 @if (!is_null($values))
-<div class="row p-0 m-0">
+<div class="row">
     @foreach ($values as $value)
-        <div class="col-2 p-0 m-0 mb-3">
+        <div class="col-2 mb-3">
             <a href="{{asset('storage/'.str_replace('public/','',$value))}}" target="_blank" style="max-height: 50px; max-width: 50px;"><img src="{{url(str_replace('public','storage',$value))}}" style="max-height: 50px; width: auto; border-radius: 3px;"></a>
         </div>
     @endforeach

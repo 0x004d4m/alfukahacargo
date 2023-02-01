b2f18b9c98e98a7c27e80c7fd27ad762b4624a25
0x004d4m <0x004d4m@gmail.com>
2023-02-02 02:08:33 +0300

    updates

--- resources/views/vendor/backpack/base/inc/topbar_left_content.blade.php
+++ resources/views/vendor/backpack/base/inc/topbar_left_content.blade.php
@@ -3,3 +3,4 @@
 {{-- <li class="nav-item px-3"><a class="nav-link" href="#">Dashboard</a></li>
 <li class="nav-item px-3"><a class="nav-link" href="#">Users</a></li>
 <li class="nav-item px-3"><a class="nav-link" href="#">Settings</a></li> --}}
+<span style="font-size: 3vw; opacity: .2;" class="nav-item px-3">Alfukahaa Cargo</span>

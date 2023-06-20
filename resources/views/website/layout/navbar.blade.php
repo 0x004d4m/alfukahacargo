7ee863bc18cf6e2ffed805d0e623886d14ceae3d
Adam Sabbah <Adam Sabbah>
2023-06-20 10:23:50 +0300

    updates

--- resources/views/website/layout/navbar.blade.php
+++ resources/views/website/layout/navbar.blade.php
@@ -5,6 +5,8 @@
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarCollapse">
+        @if (true)
+        @else
         <form action="/order" method="GET">
             <div class="input-group mb-3">
                 <input type="text" class="form-control" name="vin" placeholder="{{__('order.vin_number')}}"  required value="@if (Request::has('vin')){{Request::get('vin')}}@endif">
@@ -13,12 +15,16 @@
                 </button>
             </div>
         </form>
+        @endif
         <div class="navbar-nav ms-auto p-4 p-lg-0" dir="{{__('content.lang3')}}">
+            @if (true)
+            @else
             <a href="/#" class="nav-item nav-link active">{{__('content.home')}}</a>
             <a href="/#about" class="nav-item nav-link">{{__('content.about')}}</a>
             <a href="/#service" class="nav-item nav-link">{{__('content.services')}}</a>
             <a href="/#contact" class="nav-item nav-link">{{__('content.contact')}}</a>
             <a href="/admin" class="nav-item nav-link">{{__('content.login')}}</a>
+            @endif
             <a href="/set-language/{{__('content.lang2')}}" class="nav-item nav-link">{{__('content.lang')}}</a>
         </div>
         <h4 class="m-0 pe-lg-5 d-none d-lg-block"><i class="fa fa-headphones text-primary me-3"></i>+12164008927</h4>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,15 @@
 @extends('website.layout.main')
 @section('title', __('content.home'))
 @section('content')
-    @include('website.partials.carousel')
-    @include('website.partials.about')
-    @include('website.partials.fact')
-    @include('website.partials.service')
-    @include('website.partials.feature')
-    @include('website.partials.contact')
+    @if (true)
+        <h3 class="text-center"><br><br>Comming Soon</h3>
+        @include('website.partials.contact')
+    @else
+        @include('website.partials.carousel')
+        @include('website.partials.about')
+        @include('website.partials.fact')
+        @include('website.partials.service')
+        @include('website.partials.feature')
+        @include('website.partials.contact')
+    @endif
 @endsection
